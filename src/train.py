cf31996519672e9c8c6dd685d85abf7f841ceeee
shest <shestopalovegor@gmail.com>
2020-07-14 23:02:21 +0300

    Added Radam optimizer. Structure changed a bit

--- src/train.py
+++
@@ -1,9 +0,0 @@
-from src.data import DogClfDataset, Letterbox
-
-data_path = 'data/Images'
-annotation_path = 'data/lists/train_list.mat'
-
-resizer = Letterbox(512, 512)
-
-ds = DogClfDataset(data_path, annotation_path, resizer=resizer)
-ds[0]
