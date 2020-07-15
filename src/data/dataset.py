46bf4582d467a0244eb25d3f17f63bea501e00da
shest <shestopalovegor@gmail.com>
2020-07-15 23:50:49 +0300

    Added test_run for dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -10,12 +10,16 @@ from .utils import Normalize
 class DogClfDataset(Dataset):
 
     def __init__(self, data_path, annotation_path, transforms=None, resizer=None, means=(0.485, 0.456, 0.406),
-                 stds=(0.229, 0.224, 0.225)):
+                 stds=(0.229, 0.224, 0.225), test_run=False):
         self.data_path = data_path
         self.annotation = scipy.io.loadmat(annotation_path)
         self.tramsforms = transforms
         self.resizer = resizer
         self.normalizer = Normalize(means, stds)
+
+        if test_run:
+            self.annotation['file_list'] = self.annotation['file_list'][:300]
+            self.annotation['labels'] = self.annotation['labels'][:300]
 
     def __len__(self):
         return len(self.annotation['file_list'])
@@ -34,7 +38,7 @@ class DogClfDataset(Dataset):
         image = self.normalizer(image)
 
         image = torch.FloatTensor(image)
-        label = torch.tensor(label).long()
+        label = torch.tensor(label - 1).long()
 
         return {
             'image': image,
