5e76f875dc367c24305bc8c16b075c0faac6aa02
shest <shestopalovegor@gmail.com>
2020-07-15 23:51:15 +0300

    Added metrics

---
+++ src/training/metrics.py
@@ -0,0 +1,16 @@
+import torch
+from sklearn.metrics import precision_score
+
+
+@torch.no_grad()
+def accuracy(pred, gt):
+    pred = torch.argmax(pred, -1)
+    acc = (torch.argmax(pred, -1) == gt).sum() / len(gt)
+    return acc
+
+
+@torch.no_grad()
+def macro_average_precision(pred, gt):
+    pred = torch.argmax(pred, -1)
+    map = precision_score(gt.cpu().numpy(), pred.cpu().numpy(), average='macro')
+    return torch.tensor(map)
